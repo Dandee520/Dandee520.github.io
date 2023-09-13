1bcdd29052e383820559726ea1f9791a2c8a18a9
Danny Nguyen <danny.h.nguyen94@gmail.com>
2023-09-12 21:35:59 -0700

    Found correct config

--- firebase.js
+++ firebase.js
@@ -1,12 +1,13 @@
 // Replace these values with your Firebase project's config
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "https://websitedb-386f7-default-rtdb.firebaseio.com/",
+    apiKey: "AIzaSyDTF1KkR-hZ5HIWWxLw5YHMaRuPFdgbihA",
+    authDomain: "websitedb-386f7.firebaseapp.com",
+    databaseURL: "https://websitedb-386f7-default-rtdb.firebaseio.com",
     projectId: "websitedb-386f7",
-    storageBucket: "YOUR_STORAGE_BUCKET",
+    storageBucket: "websitedb-386f7.appspot.com",
     messagingSenderId: "263979045169",
-    appId: "YOUR_APP_ID"
+    appId: "1:263979045169:web:f260dc91f150166225a010",
+    measurementId: "G-1LG7JRGT3P"
   };
   
   // Initialize Firebase
